7066afb08c1b576fc6181c1049407c719ad1db57
Ali <38964898+seyedalidev2002@users.noreply.github.com>
2022-07-11 15:27:19 +0430

    change ffmpeg

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -132,7 +132,7 @@ class _VideoEditorState extends State<VideoEditor> {
   void _exportVideo() async {
     _exportingProgress.value = 0;
     _isExporting.value = true;
-    // NOTE: To use `-crf 1` and [VideoExportPreset] you need `ffmpeg_kit_flutter_min_gpl` package (with `ffmpeg_kit` only it won't work)
+    // NOTE: To use `-crf 1` and [VideoExportPreset] you need `ffmpeg_kit_flutter_full` package (with `ffmpeg_kit` only it won't work)
     await _controller.exportVideo(
       // preset: VideoExportPreset.medium,
       // customInstruction: "-crf 17",
